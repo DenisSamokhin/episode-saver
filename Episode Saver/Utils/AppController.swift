2be50dd971c52e179167ec1ac1b4ea4c9c434aa5
Denis Samokhin <denis.samokhin2011@gmail.com>
2016-07-19 14:55:23 +0300

    Utils added

---
+++ Episode Saver/Utils/AppController.swift
@@ -0,0 +1,14 @@
+//
+//  AppController.swift
+//  Episode Saver
+//
+//  Created by Denis on 7/19/16.
+//  Copyright © 2016 ITLions. All rights reserved.
+//
+
+import Foundation
+
+class AppController: NSObject {
+    static let sharedInstance = AppController()
+    private override init() {}
+}
---
+++ Episode Saver/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Episode Saver
+//
+//  Created by Denis on 7/19/16.
+//  Copyright © 2016 ITLions. All rights reserved.
+//
+
+import Foundation
+
+var kTMDBApiKey: NSString! = "57a3a351b90fa8d25e14a3d9fd49bf98"
+
